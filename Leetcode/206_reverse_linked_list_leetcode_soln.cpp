db34a03a9abf682c4198bef05266ec008b483937
Aarekh Mehta <66425861+r1-mehta-17@users.noreply.github.com>
2022-07-25 21:23:00 +0530

    Create 206_reverse_linked_list_leetcode_soln.cpp

---
+++ Leetcode/206_reverse_linked_list_leetcode_soln.cpp
@@ -0,0 +1,28 @@
+/*Given the head of a singly linked list, reverse the list, and return the reversed list.*/
+/*
+  Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *Previous_Node=NULL,*Current_Node=head,*Next_Node=NULL;
+        
+        while(Current_Node != NULL)
+        {
+            Next_Node = Current_Node->next;
+            Current_Node->next = Previous_Node;
+            Previous_Node = Current_Node;
+            Current_Node = Next_Node;
+        }
+        head = Previous_Node;
+        
+        return head;
+    }
+};
