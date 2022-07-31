db84813a82f454c261a4b016f466f5a31147cf2a
Aarekh Mehta <66425861+r1-mehta-17@users.noreply.github.com>
2022-07-31 11:38:44 +0530

    Create class_declaration.cpp

---
+++ oops/class_declaration.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Base Class
+class Employee {
+    //class attributes have to be made public before they are being printed in the emain function
+    public : 
+        int id;
+        float salary;
+    public : 
+        //constructor : used to initialize the elements/attributes of the class.
+        Employee(int inpId, int inpsal) {
+            id = inpId;
+            salary = inpsal;
+        }
+};
+
+/*
+Derived Class syntax :
+class {{derived-class-name}} : visibility-mode {{base-class-name}} {
+    class members,methods,etc;
+};
+//visibility mode : private, public, protected
+*/
+
+int main() {
+    // creating 2 object of class employees
+    Employee aarekh(1,100), kanav(2,101);
+    cout << aarekh.id << " : " << aarekh.salary << endl;
+    cout << kanav.id << " : " << kanav.salary << endl;
+    return 0;
+}
