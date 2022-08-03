20967e28fe08a2f8d5396068903cfd3399eb6e68
Aarekh Mehta <66425861+r1-mehta-17@users.noreply.github.com>
2022-08-03 19:28:59 +0530

    Update 141_Linked_list_cycle_leetcode_soln.cpp

--- Leetcode/141_Linked_list_cycle_leetcode_soln.cpp
+++ Leetcode/141_Linked_list_cycle_leetcode_soln.cpp
@@ -26,7 +26,25 @@ public:
             head=head->next;
         }
         return false;
-        
+    }
+};
+
+// ANOTHER FASTER APPROACH TO SOLVE THE PROBLEM :
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL)
+            return false;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow)
+                return true;
+        }
+        return false;
     }
 };
 
